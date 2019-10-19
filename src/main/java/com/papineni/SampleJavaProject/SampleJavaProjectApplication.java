5069944ae5455bcd5eed9db6ae8b9b773e179c1c
sudir81 <papinenisudheer@yahoo.com>
2019-10-19 08:46:54 -0400

    First Commit

---
+++ src/main/java/com/papineni/SampleJavaProject/SampleJavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.papineni.SampleJavaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleJavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleJavaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papineni/SampleJavaProject/SampleJavaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papineni.SampleJavaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleJavaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
